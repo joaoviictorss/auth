26c8d383a3ec95c8f4f1a2f9d12dfcc60820c741
João Victor dos Santos Silva <137708191+joaoviictorss@users.noreply.github.com>
2024-04-29 11:39:17 -0300

    feat: add login

--- actions/login.ts
+++ actions/login.ts
@@ -1,14 +1,36 @@
 "use server";
 
-import { LoginSchema } from '@/schemas';
-import * as z from 'zod'
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { LoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
+import * as z from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
-  const validateFields = LoginSchema.safeParse(values)
+  const validateFields = LoginSchema.safeParse(values);
 
-  if(!validateFields.success)  {
-    return {error: "Campos Invalidos!"}
+  if (!validateFields.success) {
+    return { error: "Campos Invalidos!" };
   }
 
-  return {sucess: "Email enviado!"}
+  const { email, password } = validateFields.data;
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Credenciais inválidas!" };
+        default:
+          return { error: "Algo deu errado :(" };
+      }
+    }
+
+    throw error;
+  }
 };
---
+++ routes.ts
@@ -0,0 +1,30 @@
+/**
+ * Um array de rotas publicas
+ * Não será necessaria a autenticação
+ * @type {string[]}
+ */
+
+export const publicRoutes = ["/"];
+
+/**
+ * Um array de rotas utilizadas para autenticação
+ * Essas rotas irão redirecionar usuarios logados para /settings
+ * @type {string[]}
+ */
+
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * O prefixo para a API de authnticação de rotas
+ * Rotas que começam com esse prefixo são usadas pela a API
+ * @type {string}
+ */
+
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * O direecionamento para usuarios logados
+ * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
